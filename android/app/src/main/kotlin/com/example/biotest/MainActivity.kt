a539f4411d77eb74a4ac43c8fb559902d23f7af5
janghanju <hanju1215@naver.com>
2024-07-25 15:46:42 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/biotest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.biotest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bio_reactor',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Bio-reactor Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late DatabaseReference _databaseReference;
+  Map<String, dynamic> _data = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _databaseReference = FirebaseDatabase.instance.ref();
+    _databaseReference.onValue.listen((event) {
+      final value = event.snapshot.value as Map<dynamic, dynamic>?;
+      setState(() {
+        if (value != null) {
+          _data = value.cast<String, dynamic>();
+        } else {
+          _data = {};
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildDataItem('temp1'),
+            _buildDataItem('temp2'),
+            _buildDataItem('temp3'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDataItem(String key) {
+    return Text(
+      '$key: ${_data[key]?.toString() ?? 'Loading...'}',
+      style: TextStyle(fontSize: 20),
+    );
+  }
+}
