412b555715a5e9efa07f6e5febb3da5754080b47
janghanju <hanju1215@naver.com>
2024-07-29 12:31:32 +0900

    그래프 일부 수정 완료 추가 수정 필요, 사이드바, 스트리밍영상 수정중

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,10 @@ import Foundation
 
 import firebase_core
 import firebase_database
-import firebase_messaging
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
