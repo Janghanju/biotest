d03fc045c5d11e749841c839d6a2d1606dd16fe1
janghanju <hanju1215@naver.com>
2024-09-09 15:03:30 +0900

    멀티챔버 호환, 블루투스 연결완료, 일부 수정완료 v1.0.1.2

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 import 'package:biotest/screens/SettingsScreen.dart';
-import 'package:biotest/screens/getItem.dart';
 import 'package:biotest/screens/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
